d4da6cf53636c00fad3eb48338a2de63eb0c4e41
Aman Kumar <macaman7822@gmail.com>
2026-03-15 22:40:43 +0530

    api.js problem solve

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,35 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', // Vite ke liye
+// });
+
+// export default API;
+
+
+
+
+
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', // Vite ke liye
+  baseURL: "https://ambulance-booking-backend-lafq.onrender.com/api"
 });
 
 export default API;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // import axios from "axios";
